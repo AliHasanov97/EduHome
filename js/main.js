67a16cb2ff02e3bb5323a4c8746075d8fd3a59d6
Aliko97 <ali.hasanov@bk.ru>
2024-12-09 12:09:19 +0400

    Updated

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ windows.on("scroll", function () {
   var scroll = windows.scrollTop();
   if (scroll < 5) {
     stick.removeClass("new-header");
+    $(".header-bottom").css("height","125px")
   } else {
     stick.addClass("new-header");
+    $(".new-header").css("height","80px")
+    $(".new-header").css("height","60px")
   }
 });
